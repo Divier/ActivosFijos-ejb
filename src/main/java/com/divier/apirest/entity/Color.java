2f663025aa5b9541618b86e107491d8eb1f9192a
Divier <diviercasas26@gmail.com>
2017-01-26 23:58:07 -0500

    commit
    commit

---
+++ src/main/java/com/divier/apirest/entity/Color.java
@@ -0,0 +1,44 @@
+package com.divier.apirest.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="color")
+public class Color implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_color")
+	private Integer id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
